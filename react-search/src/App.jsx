4a9c026bd530cad02599fe7c60a5c460b462410c
nomyard <nomyards@gmail.com>
2018-02-20 21:13:09 -0500

    updates

--- react-search/src/App.jsx
+++ react-search/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Search from './Search.jsx';
 import './App.css';
 
 class App extends Component {
@@ -6,10 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          
           <h1> Reactive-Search!</h1>
-
+          <h5> Have some fun with real-time searching! </h5>
           
         </header>
+
+          <Search />
+
       </div>
     );
   }
---
+++ react-search/src/Search.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <h3> Search Here: </h3>
+        <input type="text" value="" placeholder="Type here"/>
+
+      </div>
+    );
+  }
+}
+
+export default Search;
